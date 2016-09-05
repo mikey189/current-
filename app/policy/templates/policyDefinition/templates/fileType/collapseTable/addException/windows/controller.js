958cd6b41f40cd5662941044cd3aa8a71ba6bb50
jonathanmeguira <johnmeguira@gmail.com>
2016-09-05 10:14:07 +0300

    B"H added some updates to $mdDialog
    lazyLoad()

--- app/policy/templates/policyDefinition/templates/fileType/collapseTable/addException/windows/controller.js
+++ app/policy/templates/policyDefinition/templates/fileType/collapseTable/addException/windows/controller.js
@@ -1,24 +1,38 @@
-app.controller("windowsEXEException", ['$mdDialog', function($mdDialog){
+app.controller("windowsEXEException", ['$mdDialog', '$scope', function ($mdDialog, $scope) {
     this.test = "test ici";
-    this.closeDialog = function(){
-        $mdDialog.hide();
-    }
-    
+
     this.cancel =
-        
-        function(){
-        $mdDialog.cancel();
-        
-    }
-    
+
+        function () {
+            $mdDialog.cancel();
+
+        }
+    this.title = "Add an Exception";
     this.sizeLimit = 1;
     this.process = true;
     this.sandbox = false;
     this.users = [];
+    
     this.usersReadOnly = false;
     this.computers = [];
     this.computersReadOnly = false;
     this.groups = [];
     this.groupsReadOnly = false;
+    this.os = $scope.os;
+    this.extension = $scope.extension;
     
+    
+    this.exceptionItems = function(){
+        this.items =  {"context": $scope.clickedID, users: this.users, "groups": this.groups, "computers": this.computers}
+        console.log(this.items)
+        return this.items;
+    }
+    
+    
+    this.closeDialog = function () {
+        $mdDialog.hide();
+        this.exceptionItems();
+
+    }
+
 }])
--- app/policy/templates/policyDefinition/templates/fileType/collapseTable/controller.js
+++ app/policy/templates/policyDefinition/templates/fileType/collapseTable/controller.js
@@ -1,25 +1,50 @@
-app.controller("PDCollapseTable", ['$mdDialog', function ($mdDialog) {
+app.controller("PDCollapseTable", ['$mdDialog', '$scope', function ($mdDialog, $scope) {
 
-    this.th = ["Allow", "Size Limit", "Transform", "Process Embbeded files", "Sandbox", "Exception"]
+    this.th = ["Allow", "Size Limit", "Transform", "Process Embbeded files", "Sandbox", "Exception"];
 
+
+    //initiate $scope.clickedID to recover the value from the click and pass it to the exeption window template
+    $scope.clickedID;
+    /*getting the id from the click element and storing on the $scope to pass it to the exception window*/
+    angular.element(document).find('.getID').click(function () {
+        $scope.clickedID = this.id;
+        $scope.transform = function(){
+            $scope.filterID = $scope.clickedID.split(".");
+            $scope.os = $scope.filterID[0];
+            $scope.extension = $scope.filterID[1].toUpperCase();
+            return $scope.os;
+            return $scope.extension;
+        }
+        $scope.transform();
+        console.log($scope.clickedID);
+        return $scope.clickedID;
+        
+    });
+
+
+    //defining the method to pop up the excpetion window//
 
     this.showAdvanced = function (ev) {
+
         $mdDialog.show({
                 templateUrl: 'app/policy/templates/policyDefinition/templates/fileType/collapseTable/addException/windows/exe.html',
                 parent: angular.element(document.querySelector("#PDCollapseTable")),
                 targetEvent: ev,
-                clickOutsideToClose: false
-                //fullscreen: true // Only for -xs, -sm breakpoints.
+                clickOutsideToClose: true,
+                //exception window will inherit of the scope of this controller//
+                scope: $scope,
+                //if preserveScope is not present, then $mdDialog will only open once and will not reopen after being closed DO NOT REMOVE
+                preserveScope: true
+
             })
             .then(function (answer) {
                 this.status = 'You said the information was "' + answer + '".';
             }, function () {
                 this.status = 'You cancelled the dialog.';
             });
-        
 
     };
-   
+
 
     this.windows = [
         {
@@ -296,6 +321,7 @@ app.controller("PDCollapseTable", ['$mdDialog', function ($mdDialog) {
 
 
         ]
+
 
 
 }])
