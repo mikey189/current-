4606b8abeb5a31ee33229362109c20749167f2c1
jonathanmeguira <johnmeguira@gmail.com>
2016-11-26 18:47:26 +0200

    settings, reports, sanitization
    Settings:
    fixed md-dynamical-height
    added some styling
    Reports:
    Added styling
    Sanitiazation:
    fixed the issue with the modal ($scope.new())
    added some styling
    
    Missing:
    Update query parameters for reports

--- app/reports/sanitization/directives.js
+++ app/reports/sanitization/directives.js
@@ -1,32 +1,3 @@
-app.directive("getSantizationId", function (sanitization_factory) {
-        return {
-            restrict: "A",
-            priority: 10,
-            link: function (scope, element, attr) {
-                element.click(function () {
-                    console.log("getting id")
-                    scope.sanitization_id = element.attr("sanitizationId")
-                    console.log("just clicked "+scope.sanitization_id)
-                    scope.$apply()
-                })
-            }
-        }
-    })
-    //for some reason the more priority is elevated the later it will be executed
-app.directive("openSanitizationDetails", function () {
-    return {
-        restrict: "A",
-        priority: 10000,
-        link: function (scope, element, attrs) {
-            element.click(function () {
-               // scope.get_details()
-                scope.show_details_modal()
-
-            })
-        }
-    }
-})
-
 app.directive("closeSanitizationDetails", function ($mdDialog) {
     return {
         restrict: "A",
--- app/reports/sanitization/sanitization.js
+++ app/reports/sanitization/sanitization.js
@@ -1,5 +1,4 @@
 app.controller("sanitization", function ($scope, sanitization_factory, $mdDialog, channelData) {
-
   $scope.selected = [];
   $scope.query = {
     order: 'StartTime',
@@ -19,7 +18,6 @@ app.controller("sanitization", function ($scope, sanitization_factory, $mdDialog
     $scope.query.order = newValues[2]
     $scope.get_data()
   });
-
   //filter dialog
   $scope.limitOptions = $scope.limitOptions ? undefined : [10, 25, 50, 100];
   $scope.options = {
@@ -35,9 +33,19 @@ app.controller("sanitization", function ($scope, sanitization_factory, $mdDialog
   $scope.open_sanitization_filter = function () {
     $mdDialog.show({
       controller: "sanitization",
-      controllerAs: "ctrl",
       clickOutsideToClose: true,
       templateUrl: 'app/reports/sanitization/filter/filter.html',
+      parent: angular.element(document).find("body")
+    })
+  };
+  $scope.open_details = function () {
+    $mdDialog.show({
+      controller: "sanitization",
+      /*without this, for some crazy and unknown reason, this particular dialog does inherit only
+    scope values created at load, all scope value created after are not taken into account*/
+      scope: $scope.$new(),
+      clickOutsideToClose: true,
+      templateUrl: 'app/reports/sanitization/details/details.html',
       parent: angular.element(document).find("body")
     })
   };
@@ -52,29 +60,22 @@ app.controller("sanitization", function ($scope, sanitization_factory, $mdDialog
     })
     //end of filter diaglog
     //details dialog
-  $scope.show_details_modal = function () {
-    $mdDialog.show({
-      controller: "sanitization",
-      templateUrl: 'app/reports/sanitization/details/details.html',
-      parent: angular.element(document).find("body")
-    })
-  };
-
   $scope.send_filter_query = function () {
     $mdDialog.hide()
     sanitization_factory.get_filter_results($scope.f_q).then(function (answer) {
       $scope.data = answer.data
-
     })
   }
   $scope.cancel_filter = function () {
-    $mdDialog.cancel()
-  }
-  $scope.get_details = function (id) {
+      $mdDialog.cancel()
+    }
+    /*whoever you are do never touch that piece of code, dialog from the angular material team work in 
+     a very weird and unstable way, thus do not change the local version of angular material and do not
+    not even think about doing some update : current version RC1*/
+  $scope.show_details_modal_ready = function (id) {
     sanitization_factory.get_details(id).then(function (answer) {
       $scope.sanitization_details = answer.data
-      console.log(JSON.stringify($scope.sanitization_details))
     })
+    $scope.open_details()
   }
-
 });
