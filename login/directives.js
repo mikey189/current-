a0d56bad3bb0b9964b7f39115c88dbd3f3c3f3dd
jonathanmeguira <johnmeguira@gmail.com>
2017-01-11 15:52:38 +0200

    login with token

--- login/directives.js
+++ login/directives.js
@@ -1,26 +1,29 @@
+app.directive("checkCredentials", ["authService", "$rootScope", "$http", "$state", "$timeout",
+    function (authService, $rootScope, $http, $state, $timeout) {
+        return {
+            restrict: "A",
+            link: function (scope, element, attrs) {
+                element.bind("click", function () {
+                    var serverName = document.getElementById("ServerName").value
+                    var username = $("#username")
+                    var password = $("#password")
+                    authService.checkLogin(scope.ctrl.serverName, scope.ctrl.UserName, scope.ctrl.Password).then(function (answer) {
+                        var token = "Bearer " + answer.data.AccessToken
+                        localStorage.setItem("serverName", serverName)
+                        localStorage.setItem("token", token);
+                        $timeout(function () {
+                            $state.go("app.dashboard")
 
-app.directive("checkCredentials", ["authService", "$rootScope", "$http", "$state",
- function (authService, $rootScope, $http, $state) {
-    return {
-        restrict: "A",
-        link: function (scope, element, attrs) {
-            element.bind("click", function () {
-                var serverName = document.getElementById("ServerName").value
-                var username = $("#username")
-                var password = $("#password")           
-                authService.checkLogin(scope.ctrl.serverName, scope.ctrl.UserName, scope.ctrl.Password).then(function (answer) {
-                    var token = "Bearer " + answer.data.AccessToken
-                    localStorage.setItem("serverName", serverName)
-                    localStorage.setItem("token", token);
-                    $state.go("app.dashboard")
+                        })
 
-                }, function (error) {
-                    username.addClass("error animated shake")
-                    password.addClass("error animated shake")
-                    scope.ctrl.is_login_nahon = false
+                    }, function (error) {
+                        username.addClass("error animated shake")
+                        password.addClass("error animated shake")
+                        scope.ctrl.is_login_nahon = false
+                    })
                 })
-            })
 
+            }
         }
     }
-}]);
+]);
--- login/login.js
+++ login/login.js
@@ -1,4 +1,4 @@
-app.controller("login", ["$rootScope", "authService","$state", function ($rootScope, authService, $state) {
+app.controller("login", ["$rootScope", "authService", "$state", "$timeout", function ($rootScope, authService, $state, $timeout) {
     var self = this;
     self.is_login_nahon = true
 
@@ -15,8 +15,11 @@ app.controller("login", ["$rootScope", "authService","$state", function ($rootSc
                 var token = "Bearer " + answer.data.AccessToken
                 localStorage.setItem("serverName", serverName)
                 localStorage.setItem("token", token);
-                $state.go("app.dashboard")
+                console.log(token)
+                $timeout(function () {
+                    $state.go("app.dashboard")
 
+                })
             }, function (error) {
                 username.addClass("error animated shake")
                 password.addClass("error animated shake")
