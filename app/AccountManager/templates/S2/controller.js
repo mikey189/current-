e243806975f01c161616a3299d75c32f1f8c46d3
jonathanmeguira <johnmeguira@gmail.com>
2017-03-02 12:50:14 +0200

    Reset Password S2 => Disable password type (password : text)

--- app/AccountManager/templates/S2/controller.js
+++ app/AccountManager/templates/S2/controller.js
@@ -1,5 +1,8 @@
 app.controller("ResetPasswordS2", function () {
     var self = this;
+    self.showPassword1 = true;
+    self.showPassword2 = true;
+
     self.ConfirmNewEmail = () => {
         //wait until server post and then inside success go to login otherwise prompt error message
         $state.go("login");
