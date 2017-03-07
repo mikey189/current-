0d9dd82db8f995277410f1367c15e4c74b7ec0ad
jonathanmeguira <johnmeguira@gmail.com>
2017-03-07 17:57:57 +0200

    added date filter for sanitization

--- app/reports/email/email.js
+++ app/reports/email/email.js
@@ -1,47 +1,48 @@
-app.controller("reports_emails", function($scope, sanitization_factory,emails_factory, $mdDialog){
+app.controller("reports_emails", function ($scope, sanitization_factory, emails_factory, $mdDialog) {
 
-    $scope.email_query = {
-        order: 'Time',
-        PageSize: 10,
-        PageIndex: 1
-    }
-    
-    $scope.get_data = function () {
-        emails_factory.get_emails($scope.email_query.PageIndex, $scope.email_query.PageSize, $scope.email_query.order).then(function (answer) {
-            $scope.data = answer.data
-            $scope.total_length = answer.data.Total
-        })
-    }
-     $scope.LoadActions = (ID) => {
+  $scope.email_query = {
+    order: 'Time',
+    PageSize: 10,
+    PageIndex: 1
+  }
+
+  $scope.get_data = function () {
+    emails_factory.get_emails($scope.email_query.PageIndex, $scope.email_query.PageSize, $scope.email_query.order).then(function (answer) {
+      $scope.data = answer.data
+      $scope.total_length = answer.data.Total
+    })
+  }
+  $scope.LoadActions = (ID) => {
     sanitization_factory.get_actions(ID)
       .then((results) => {
         $scope.actions = results.data;
       })
   };
-    $scope.$watchGroup(['$scope.email_query.PageIndex', '$scope.email_query.PageSize', '$scope.email_query.order'], function (newValues, oldValues, scope) {
-        $scope.email_query.PageIndex = newValues[0]
-        $scope.email_query.PageSize = newValues[1]
-        $scope.email_query.order = newValues[2]
-        $scope.get_data()
-    });
-    //filter dialog
-    $scope.limitOptions = $scope.limitOptions ? undefined : [10, 25, 50, 100];
+  $scope.$watchGroup(['$scope.email_query.PageIndex', '$scope.email_query.PageSize', '$scope.email_query.order'], function (newValues, oldValues, scope) {
+    $scope.email_query.PageIndex = newValues[0]
+    $scope.email_query.PageSize = newValues[1]
+    $scope.email_query.order = newValues[2]
+    $scope.get_data()
+  });
+  //filter dialog
+  $scope.limitOptions = $scope.limitOptions ? undefined : [10, 25, 50, 100];
 
-    $scope.options = {
-        rowSelection: true,
-        multiSelect: true,
-        autoSelect: true,
-        decapitate: false,
-        largeEditDialog: false,
-        boundaryLinks: false,
-        limitSelect: true,
-        pageSelect: true
-    }
+  $scope.options = {
+    rowSelection: true,
+    multiSelect: true,
+    autoSelect: true,
+    decapitate: false,
+    largeEditDialog: false,
+    boundaryLinks: false,
+    limitSelect: true,
+    pageSelect: true
+  }
 
 
-     $scope.open_sanitization_filter = function () {
+  $scope.open_sanitization_filter = function () {
     $mdDialog.show({
-      controller: "sanitization",
+      scope: $scope, // use parent scope in template
+      preserveScope: true,
       clickOutsideToClose: true,
       templateUrl: 'app/reports/sanitization/filter/filter.html',
       parent: angular.element(document).find("body")
@@ -49,7 +50,8 @@ app.controller("reports_emails", function($scope, sanitization_factory,emails_fa
   };
   $scope.open_details = function () {
     $mdDialog.show({
-      controller: "sanitization",
+      scope: $scope, // use parent scope in template
+      preserveScope: true,
       /*without this, for some crazy and unknown reason, this particular dialog does inherit only
     scope values created at load, all scope value created after are not taken into account*/
       scope: $scope.$new(),
@@ -62,11 +64,11 @@ app.controller("reports_emails", function($scope, sanitization_factory,emails_fa
     $scope.sanitization_fields = answer.data
   })
 
-   $scope.show_details_modal_ready = function (id) {
+  $scope.show_details_modal_ready = function (id) {
     sanitization_factory.get_details(id).then(function (answer) {
       $scope.sanitization_details = answer.data
     })
     $scope.open_details()
   };
-    
+
 })
